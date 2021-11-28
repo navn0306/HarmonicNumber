f2039552d09d4eeff24e0a685057dd1cc2baefc2
Navneet <navn0306@gmail.com>
2021-11-28 12:58:36 +0530

    ADD: Added welcome message for the program.

---
+++ src/com/bridge/HarmonicNumber/HarmonicNumber.java
@@ -0,0 +1,8 @@
+package com.bridge.HarmonicNumber;
+
+public class HarmonicNumber {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome, This program prints the nth harmoinic value for a given number" );
+    }
+}
